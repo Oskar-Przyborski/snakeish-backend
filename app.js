94430d98469d090d3995a9308f59a33613e391e9
Oskar-Przyborski <oskar.przski@gmail.com>
2022-04-04 14:18:00 +0200

    cors fix2

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ import gamesManager from './gamesManager.js'
 const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: "Access-Control-Allow-Origin"
     }
 });
 
