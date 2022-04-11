8408b1c89f0374b1c7a5a0b7b67f6aa7ca1102c4
Oskar-Przyborski <oskar.przski@gmail.com>
2022-04-11 17:36:56 +0200

    using isnamealreadytaken when joining game

--- Classes/Player.js
+++ Classes/Player.js
@@ -24,6 +24,7 @@ export default class Player {
         if (name == null) { callback(true, "name not specified"); return; }
         if (name == "") { callback(true, "name is empty"); return; }
         if (name.length > 10) { callback(true, "name should be shorter than 11 characters"); return; }
+        if (this.room.IsNameAlreadyTaken(name)) { callback(true, "name is already taken"); return; }
         if (colorIDX == null) { callback(true, "color not specified"); return; }
         if (colorIDX < 0 || colorIDX > 6) { callback(true, "color is out of range"); return; }
         if (this.isPlaying) { callback(true, "already playing"); return; }
