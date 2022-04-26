8e63b080820f0be60d7e757fe8bae09a9657001d
Oskar-Przyborski <oskar.przski@gmail.com>
2022-04-26 22:14:08 +0200

    colors fix

--- Classes/Player.js
+++ Classes/Player.js
@@ -27,7 +27,7 @@ export default class Player {
         if (name.length > 10) { callback(true, "name should be shorter than 11 characters"); return; }
         if (this.room.IsNameAlreadyTaken(name)) { callback(true, "name is already taken"); return; }
         if (colorIDX == null) { callback(true, "color not specified"); return; }
-        if (colorIDX < 0 || colorIDX > 6) { callback(true, "color is out of range"); return; }
+        if (colorIDX < 0 || colorIDX > 11) { callback(true, "color is out of range"); return; }
         if (this.isPlaying) { callback(true, "already playing"); return; }
         if (!this.room.gameMode.CanJoinGame(this)) { callback(true, "cannot join game"); return; }
         this.gameData = this.room.gameMode.CreatePlayerData(this, name, snakeColors[colorIDX]);
